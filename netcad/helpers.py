7977efcde41755fad169c7eab17a4d13c5796f03
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-10-10 15:54:34 -0400

    update

--- netcad/helpers.py
+++ netcad/helpers.py
@@ -3,11 +3,28 @@ from itertools import groupby
 
 
 def vlan_range_string(vlan_ids: List[int]) -> str:
+    """
+    Given a *sorted* list of numbers (VLAN-IDs), return a string that
+    converts consecuitve numbers into comma separated ranges.  For example:
+        [1,2,3,4,5,7,10,11,12] -> "1-5,7,10-12"
+
+    Parameters
+    ----------
+    vlan_ids: List[int]
+        The *sorted* list of vlan ID numbers.
+
+    Returns
+    -------
+    The string as described.
+    """
+
     range_strings = list()
 
     for _, num_gen in groupby(enumerate(vlan_ids), key=lambda x: x[0] - x[1]):
-        num_list = [item[1] for item in num_gen]
+        num_list = tuple(num_gen)
         first, last = num_list[0], num_list[-1]
-        range_strings.append(str(first) if first == last else f"{first}-{last}")
+        range_strings.append(
+            str(first[1]) if first is last else f"{first[1]}-{last[1]}"
+        )
 
     return ",".join(range_strings)
