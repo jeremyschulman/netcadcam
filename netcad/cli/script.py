0b2a1fbe32da6f85cbc80db80fc42aef9439191b
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-10-18 20:39:35 -0400

    update

--- netcad/cli/script.py
+++ netcad/cli/script.py
@@ -7,10 +7,16 @@ from .main import cli
 def script():
     try:
         cli()
+
     except RuntimeError as exc:
         if ("--debug" in sys.argv) or os.getenv("NETCAD_DEBUG"):
             import traceback
 
             traceback.print_exc()
 
-        sys.exit("ERROR: " + exc.args[0])
+        obj_data = ''
+        if len(exc.args) > 1:
+            from pprint import pformat as pp
+            obj_data = '\n'.join([pp(obj) for obj in exc.args[1:]])
+
+        sys.exit("ERROR: " + exc.args[0] + "\n" + obj_data)
