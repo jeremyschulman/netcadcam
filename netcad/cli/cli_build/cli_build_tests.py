a9e1d346f342dcf82165e87e1dc6d7afa55900fc
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-10-22 10:40:07 -0400

    update

--- netcad/cli/cli_build/cli_build_tests.py
+++ netcad/cli/cli_build/cli_build_tests.py
@@ -2,8 +2,10 @@
 # System Imports
 # -----------------------------------------------------------------------------
 
-from typing import Tuple
+from typing import Tuple, Dict, List
+import asyncio
 from pathlib import Path
+from logging import Logger
 
 # -----------------------------------------------------------------------------
 # Public Imports
@@ -15,8 +17,35 @@ import click
 # Private Imports
 # -----------------------------------------------------------------------------
 
+from netcad.logger import get_logger
+from netcad.device import Device
+from netcad.config import Environment
+from netcad.testing import TestingServices, TestCases
+
 from netcad.cli.main import clig_build
 from netcad.cli.common_opts import opt_devices, opt_network
+
+# -----------------------------------------------------------------------------
+# Exports (none)
+# -----------------------------------------------------------------------------
+
+__all__ = []
+
+
+# -----------------------------------------------------------------------------
+#
+#                                 CODE BEGINS
+#
+# -----------------------------------------------------------------------------
+
+DevicesList = List[Device]
+AvailableTestCases = Dict[str, TestCases]
+
+
+async def build_device_tests(
+    device: Device, available_test_cases: AvailableTestCases, log: Logger
+):
+    log.info(f"Building tests for device: {device.name}")
 
 
 @clig_build.command(name="tests")
@@ -26,11 +55,9 @@ from netcad.cli.common_opts import opt_devices, opt_network
     "--tests-dir",
     help="location to store configs",
     type=click.Path(path_type=Path, resolve_path=True, exists=True, writable=True),
-    default="tests",
+    envvar=Environment.NETCAD_TESTCASESDIR,
 )
-def cli_build_tests(
-    devices: Tuple[str], networks: Tuple[str], configs_dir: Path, output_console: bool
-):
+def cli_build_tests(devices: Tuple[str], networks: Tuple[str], tests_dir: Path):
     """
     Build device test cases to audit live network
 
@@ -44,10 +71,41 @@ def cli_build_tests(
     ----------
     devices
     networks
-    configs_dir
-    output_console
+    tests_dir
 
     Returns
     -------
     """
-    pass
+    log = get_logger()
+
+    # The set of devices we will build configurations for.  This set will be
+    # sorted before the rendering process begins.
+
+    device_objs = set()
+
+    if devices:
+        for each_name in devices:
+            if not (dev_obj := Device.registry_get(name=each_name)):
+                log.error(f"Device not found: {each_name}")
+                return
+
+            device_objs.add(dev_obj)
+
+    device_objs = list(device_objs)
+    device_objs.sort()
+
+    log.info(f"Building device audits for {len(devices)} devices")
+    available_test_cases = TestingServices.registry_items()
+
+    async def run():
+        tasks = [
+            asyncio.create_task(
+                build_device_tests(
+                    device=device, available_test_cases=available_test_cases, log=log
+                )
+            )
+            for device in device_objs
+        ]
+        await asyncio.gather(*tasks)
+
+    asyncio.run(run())
