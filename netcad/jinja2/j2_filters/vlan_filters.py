eafb68c8ad03571d9f512fe65a1669417c7d2427
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-11-26 15:37:42 -0500

    update

--- netcad/jinja2/j2_filters/vlan_filters.py
+++ netcad/jinja2/j2_filters/vlan_filters.py
@@ -30,4 +30,4 @@ def j2_vlans_id_list(if_obj: "DeviceInterface") -> str:
         if_name = if_obj.name
         raise RuntimeError(f"{dev_name}:{if_name} does not have assigned vlans")
 
-    return range_string(numbers=[vlan.vlan_id for vlan in vlans])
+    return range_string(numbers=sorted([vlan.vlan_id for vlan in vlans]))
