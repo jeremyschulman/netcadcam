c8e6080d64cf13e1cbcc1dd5f3516616a286586c
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-12-05 16:25:21 -0500

    update

--- netcad/cli/device_inventory.py
+++ netcad/cli/device_inventory.py
@@ -2,14 +2,13 @@
 # System Imports
 # -----------------------------------------------------------------------------
 
-from typing import Set, Sequence, List, Optional
+from typing import Sequence, List, Optional
 
 # -----------------------------------------------------------------------------
 # Private Imports
 # -----------------------------------------------------------------------------
 
 from netcad.device import Device
-from netcad.cabling import CablePlanner
 from netcad.init import load_design
 
 # -----------------------------------------------------------------------------
@@ -41,39 +40,3 @@ def get_devices_from_designs(
         return device_objs
 
     return sorted([obj for obj in device_objs if obj.name in include_devices])
-
-
-# TODO: remove
-def get_network_devices(networks: Sequence[str]) -> Set[Device]:
-    """
-    Returns a set of Device objects that are members in any of the provided
-    networks.
-
-    Parameters
-    ----------
-    networks: sequence[str]
-        Set or list of strings
-
-    Returns
-    -------
-    Set[Devices]
-
-    Raises
-    ------
-    RuntimeError if any given device does not exist in the netcad inventory.
-    """
-
-    device_objs = set()
-
-    for each_network in networks:
-        cabler: CablePlanner
-
-        if not (cabler := CablePlanner.registry_get(name=each_network)):
-            raise RuntimeError(f"Network not found: {each_network}")
-
-        if not cabler.devices:
-            raise RuntimeError(f"No devices found in network: {each_network}")
-
-        device_objs.update(cabler.devices)
-
-    return device_objs
