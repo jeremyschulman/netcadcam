238735e6c681c393e6db38acc1e558313953c40f
Jeremy Schulman <jeremy@apstra.com>
2021-12-08 20:11:30 -0500

    Jls dev dec08 (#46)

--- netcad/device/device_interface.py
+++ netcad/device/device_interface.py
@@ -106,8 +106,10 @@ class DeviceInterface(object):
         # interface attributes accordinly.
 
         if name.isdigit():
-            self.port_numbers = int(name)
-            self.sort_key = self.port_numbers
+            port_id = int(name)
+            self.port_numbers = (port_id,)
+            # need the first item in the tuple to be any string value for sorting purposes
+            self.sort_key = ("port", port_id)
             self.short_name = name
 
         # if the name contains numbers then break these numbers out for
@@ -122,7 +124,7 @@ class DeviceInterface(object):
         # then set the number value to 0 for default purposes.
         else:
             self.short_name = name
-            self.sort_key = 0
+            self.sort_key = (name, 0)
             self.port_numbers = None
 
         self._profile = None
