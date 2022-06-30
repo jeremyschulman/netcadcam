37dfca423101faa65de5813fb0a0680f1e2ffac1
Jeremy Schulman <jeremy@apstra.com>
2022-06-30 13:45:54 -0400

    Jls dev jun30 (#92)

--- netcad/cli/netcam/show_checks/find_check_services.py
+++ netcad/cli/netcam/show_checks/find_check_services.py
@@ -1,8 +1,14 @@
 def find_check_services(device, optionals: dict):
 
     inc_check_names = optionals["testing_service_names"]
+    inc_service_names = optionals["service_list"]
 
     for design_service in device.services.values():
+
+        if inc_service_names:
+            if design_service.name not in inc_service_names:
+                continue
+
         for check_service in design_service.check_collections:
             check_name = check_service.get_name()
 
