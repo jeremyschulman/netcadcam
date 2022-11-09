4818de1bf7fdeb2ff680ad9bfd7fa20912ff295d
Jeremy Schulman <nwkautomaniac@gmail.com>
2022-11-09 15:04:03 -0500

    updates

--- netcad/topology/checks/check_lags.py
+++ netcad/topology/checks/check_lags.py
@@ -125,7 +125,8 @@ class LagCheckCollection(CheckCollection):
         # return the collection sorted by the lag interface name
 
         collection.checks.sort(
-            key=lambda tc: DeviceInterface(tc.check_params.interface)
+            key=lambda tc: DeviceInterface(tc.check_params.interface,
+                                           interfaces=device.interfaces)
         )
 
         return collection
