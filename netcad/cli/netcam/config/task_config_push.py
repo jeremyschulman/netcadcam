51921334916b30b9e7d7a5f3abe3f871ea6ea259
Jeremy Schulman <nwkautomaniac@gmail.com>
2023-02-10 13:30:07 -0500

    updates

--- netcad/cli/netcam/config/task_config_push.py
+++ netcad/cli/netcam/config/task_config_push.py
@@ -1,4 +1,9 @@
+import aiofiles
+
+from netcad.logger import get_logger
 from netcad.netcam.dev_config import AsyncDeviceConfigurable
+
+
 from .deco_temp_file import temp_file
 
 _OK_ = "[green]OK:[/green]"
@@ -8,4 +13,18 @@ _FAIL_ = "[red]FAIL:[/red]"
 
 @temp_file
 async def push_device_config(dev_cfg: AsyncDeviceConfigurable, rollback_timeout: int):
+    name = dev_cfg.device.name
+    log = get_logger()
+    log.info(f"{name}: deploying config ...")
     await dev_cfg.config_push(rollback_timeout=rollback_timeout)
+
+    if not dev_cfg.config_diff_contents:
+        log.info(f"{name}: no config differences, nothig changed.")
+        return
+
+    log.info(f"{name}: config active and saved to startup")
+
+    diff_file = dev_cfg.config_file.parent.joinpath("diffs") / dev_cfg.config_file.name
+    async with aiofiles.open(diff_file, "w+") as ofile:
+        log.info(f"{name}: saving config-diff: {ofile.name}")
+        await ofile.write(dev_cfg.config_diff_contents)
