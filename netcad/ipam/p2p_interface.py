f59f32b3d7fc54987a39c4f4fa6dc3742ea82891
Jeremy Schulman <nwkautomaniac@gmail.com>
2023-03-04 14:25:30 -0500

    update to IPAM

--- netcad/ipam/p2p_interface.py
+++ netcad/ipam/p2p_interface.py
@@ -2,10 +2,9 @@
 # System Imports
 # -----------------------------------------------------------------------------
 
-from typing import List
+from typing import Tuple
 from ipaddress import IPv4Network, IPv4Interface
 from collections import UserDict
-from itertools import islice
 
 
 class P2PInterfaces(UserDict):
@@ -48,26 +47,69 @@ class P2PInterfaces(UserDict):
         self.network = IPv4Network(network)
         self.octet_mode = octet_mode
 
-    def __missing__(self, key: int) -> List[IPv4Interface]:
+    def __missing__(self, key: int | tuple[int], new_prefix=31) -> Tuple[IPv4Interface]:
         """
-        Returns a list of two IPv4 Interface instances with /31 prefixlen. The
-        first is the "even" numbered interface and the second is the "odd".
+        Returns a tuple of two IPv4 Interface instances with /31 prefixlen
+        whose values are the offset from the network address.  The first is the
+        "even" numbered interface and the second is the "odd".
 
         Parameters
         ----------
         key: int
             Identifies which /31 pair of interfaces to retrieve, as described.
+            For example, if the subnet was "192.168.10.0/24" and the key was
+            22, then the returned IPs would be IPInterface instances with
+            values: (192.168.10.22, 192.168.10.23)
 
-        Returns
-        -------
-        list as described
+        key: tuple[int]
+            When the key is a tuple of integers each value represts the offset
+            of that octet in the subnet.  For example if the subnet was
+            "192.168.0.0/16" and the key was the tupe (2, 10), then the
+            returned IPs would be IPv4Interface instance with values:
+            (192.168.2.10/16, 192.168.2.11/16)
         """
 
-        index, rem = divmod(key, 2) if self.octet_mode else (key, 0)
+        # if the key is not a tuple, then shape it into one.
+        is_key_tup = isinstance(key, tuple)
 
-        search = islice(self.network.subnets(new_prefix=31), index, index + 1)
+        if not is_key_tup:
+            key = (key,)
 
-        if not (net := next(search)):
-            raise RuntimeError(f"{key} not found in {self.network}")
+        if len(key) > 4:
+            raise ValueError("Invalid key length, must be <= 4")
 
-        return [IPv4Interface((host, net.prefixlen)) for host in net.hosts()]
+        key_offset = sum(
+            (256**pow_256) * number for pow_256, number in enumerate(reversed(key))
+        )
+
+        netaddr_offset, is_odd = divmod(key_offset, 2)
+
+        p2p_subnet = IPv4Network(
+            (self.network.network_address + (netaddr_offset * 2), new_prefix)
+        )
+
+        if not self.network.supernet_of(p2p_subnet):
+            raise ValueError(f"{key} not a subnet of {self.network}")
+
+        p2p_ifs = tuple(
+            IPv4Interface((host, p2p_subnet.prefixlen)) for host in p2p_subnet.hosts()
+        )
+
+        # save the key-value for future lookups.  if the given key was a tuple,
+        # then form the mate-key as a non-tuple.
+
+        if not is_key_tup:
+            given_key = key[0]
+            mate_key = given_key + (-1 if is_odd else 1)
+            self.data[given_key] = self.data[mate_key] = p2p_ifs
+
+        # otherwise, the given key was a tuple, and we need to store the
+        # mate-key also as tuple.
+
+        else:
+            given_key = list(key)
+            self.data[key] = p2p_subnet
+            given_key[-1] += -1 if is_odd else 1
+            self.data[tuple(given_key)] = p2p_ifs
+
+        return p2p_ifs
