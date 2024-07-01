4b50a528303e1b2bf6e6ff91a438ae877dd6fa72
Jeremy Schulman <nwkautomaniac@gmail.com>
2024-07-01 11:57:54 -0400

    revamp

--- netcad/device/device_decl.py
+++ netcad/device/device_decl.py
@@ -1,15 +1,20 @@
-from pydantic import BaseModel, Field
+import json
+
+from pydantic import BaseModel, Field, ValidationError
 
 from netcad.logger import get_logger
 from .device import DeviceKindRegistry, Device
 
 
 class DeviceDecl(BaseModel):
-    alias: str
-    kind: str
-    hostname: str
+    alias: str = Field(description="User-friendly name for the device")
+    profile: str = Field(description="Indentifies the Device subclass")
+    hostname: str = Field(description="Hostname or IP address of the device")
+    primary_ip: str | None = Field(None, description="Primary IP address of the device")
+
+    # identifies the list of interface alias->if_name mappings that this
+    # device will be using.
     use_interface_maps: list[str] | None = Field(default_factory=list)
-    primary_ip: str | None = Field(None)
 
 
 def build_devices_from_config(
@@ -18,12 +23,22 @@ def build_devices_from_config(
 ) -> dict[str, Device]:
     log = get_logger()
     devices = dict()
+    error_c = 0
 
     for dev_cfg in device_configs:
-        dev_decl = DeviceDecl.parse_obj(dev_cfg)
+        try:
+            dev_decl = DeviceDecl.parse_obj(dev_cfg)
+        except ValidationError as exc:
+            log.error(
+                "Error parsing device declaration: %s",
+                json.dumps(exc.errors(), indent=3),
+            )
+            error_c += 1
+            continue
 
-        if not (kind_cls := DeviceKindRegistry.get(dev_decl.kind)):
-            log.error("Unknown device kind '%s'", dev_decl.kind)
+        if not (kind_cls := DeviceKindRegistry.get(dev_decl.profile)):
+            log.error("Unknown device kind '%s'", dev_decl.profile)
+            error_c += 1
             continue
 
         dev: Device
@@ -36,5 +51,8 @@ def build_devices_from_config(
             dev.primary_ip = dev_decl.primary_ip
 
         devices[dev.alias] = dev
+
+    if error_c:
+        raise RuntimeError("Errors loading device configurations, aborting.")
 
     return devices
