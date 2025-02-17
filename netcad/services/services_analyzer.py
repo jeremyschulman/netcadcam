050555f9a0cef0ea68bf917d8ae07b2a0e59f81e
Jeremy Schulman <nwkautomaniac@gmail.com>
2025-02-17 17:32:42 -0500

    adding db

--- netcad/services/services_analyzer.py
+++ netcad/services/services_analyzer.py
@@ -28,10 +28,12 @@ if TYPE_CHECKING:
     from netcad.design import Design, DesignFeature
 
 from netcam.db import db_connect, db_tables
-from netcam.db.db_check_results import db_check_results_get
+from netcam.db.db_feature_checks import db_check_results_get
+from netcam.db.db_service_checks import db_service_check_save
 
 from ..checks import CheckCollectionT, CheckStatus
 from .design_service import DesignService
+from .service_check import DesignServiceCheck
 from .services_typedefs import ResultMapT, NodeObjIDMapT
 
 
@@ -317,6 +319,23 @@ class ServicesAnalyzer:
 
         filter_by = {k: kwargs[k] for k in key}
         return self.db.query(table).filter_by(**filter_by).first()
+
+    def db_save_service_check(
+        self, service: DesignService, check: "DesignServiceCheck"
+    ):
+        db_service_check_save(
+            db=self.db, service=service, check=check, node=self.nodes_map[check]
+        )
+        # self.db_upsert(
+        #     table=db_tables.ServiceCheckResultTable,
+        #     key=["service", "check_type", "check_id"],
+        #     node_id=node.index,
+        #     service=service.name,
+        #     check_type=check.check_type,
+        #     check_id=check.check_id,
+        #     ok=check.ok,
+        #     result=check.model_dump(),
+        # )
 
     def db_find(self, table, **filter_by):
         return self.db.query(table).filter_by(**filter_by).first()
